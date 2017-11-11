20d5f1a0db0cf0c6e830ad0bf43e7b26377e6f02
peterzhang <zhh520@gmail.com>
2017-11-11 04:58:56 +0100

    Added config module

---
+++ lib/config.py
@@ -0,0 +1,17 @@
+import os
+import yaml
+
+ROOT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+settings = {}
+cfg_file = "%s/config/app.yml" % ROOT_PATH
+
+if os.path.isfile(cfg_file):
+    settings = yaml.safe_load(open(cfg_file))
+else:
+    raise IOError("Not found config file")
+
+def get(key):
+    if key in ["database", "screenshots", "chromedriver"]:
+        if not os.path.isabs(settings[key]):
+            settings[key] = "%s/%s" % (ROOT_PATH, settings[key])
+    return settings[key]
