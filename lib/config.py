63ce7270025e261021129b0361563b26f36ec5a1
peterzhang <zhh520@gmail.com>
2017-12-17 20:02:54 +0100

    Fixed no default config issue

--- lib/config.py
+++ lib/config.py
@@ -4,11 +4,14 @@ import yaml
 ROOT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 settings = {}
 cfg_file = "%s/config/app.yml" % ROOT_PATH
+cfg_example = "%s/config/app.example.yml" % ROOT_PATH
 
 if os.path.isfile(cfg_file):
     settings = yaml.safe_load(open(cfg_file))
+elif os.path.isfile(cfg_example):
+    settings = yaml.safe_load(open(cfg_example))
 else:
-    raise IOError("Not found config file")
+    raise IOError("Not found config file '%s'" % cfg_file)
 
 def get(key):
     if key in ["database", "images", "screenshots", "chromedriver"]:
